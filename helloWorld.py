81f627c9c9aaf85f8987c0727a169e2b7b11a1bf
Dinngger <814089338@qq.com>
2019-03-24 18:21:36 +0800

    continuous read pictures

--- helloWorld.py
+++ helloWorld.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 mnist_test = open("mnist_test.csv", 'r')
-line = mnist_test.readline()
+plt.ion()
+plt.figure(1)
+for i in range(100):
+    line = mnist_test.readline()
+    pic = line.split(',')
+    num = pic[0]
+    a = np.asarray(pic)
+    a = a[1:]
+    a = a.reshape(28, 28)
+    a = a.astype(np.float64)
+    a = a / 255
+    plt.clf()
+    plt.imshow(a)
+    print(num)
+    plt.pause(1)
 mnist_test.close()
-pic = line.split(',')
-num = pic[0]
-a = np.asarray(pic)
-a = a[1:]
-a = a.reshape(28, 28)
-a = a.astype(np.float64)
-a = a / 255
-
-plt.imshow(a)
-plt.show()
